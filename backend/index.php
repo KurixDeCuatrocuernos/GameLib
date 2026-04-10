c05d06870f416cb0dbc95229f0766e93b6cecaa9
KurixDeCuatrocuernos <168125007+KurixDeCuatrocuernos@users.noreply.github.com>
2026-04-10 23:58:25 +0200

    Creación de páginas de prueba
    Creé páginas temporales de PHP para poder probar las funciones y ver su resultado.

---
+++ backend/index.php
@@ -0,0 +1,14 @@
+<!-- Esta página es temporal, ya que del front-end se encarga REACT -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Back-End</title>
+</head>
+<body>
+    <h1>Hola, estás en el Backend del TFG</h1>
+    <br>
+    <h2><a href="./pages/usersPage.php">Utilities for users</a></h2>
+</body>
+</html>
