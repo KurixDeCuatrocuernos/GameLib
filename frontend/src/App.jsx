9ff01b31f5988bbc88d29584befa8f23afdc6b51
KurixDeCuatrocuernos <168125007+KurixDeCuatrocuernos@users.noreply.github.com>
2026-04-11 21:00:35 +0200

    Creación del frontend
    Se creó el proyecto React, y se agregaron los módulos Tailwind y React Router, además se borró parte del contenido autogenerado por Vite.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-center'>Hola, este es el inicio</h1>
+    </>
+  )
+}
+
+export default App
