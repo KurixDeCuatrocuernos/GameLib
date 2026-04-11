802c9102118df76b87a5f1e5dd788cf1b6972e3f
KurixDeCuatrocuernos <168125007+KurixDeCuatrocuernos@users.noreply.github.com>
2026-04-11 20:57:11 +0200

    Creación de userFunctions.php
    Se crearon las funciones auxiliares para usuarios, como acceder al lenguaje de users_data, recoger todos los temas, entre otros

--- backend/functions/userFunctions/userFunctions.php
+++ backend/functions/userFunctions/userFunctions.php
@@ -10,6 +10,38 @@ function getRoleIdByName($roleName) {
     $result = ejecutarQuery($sql,[$roleName]);
     return $result;
 }
+/**
+ * Esta función devuelve el theme-name a partir del id de usuario
+ */
+function getThemeNameByUserId ($userId) {
+    $sql = 'SELECT th.name FROM users_data ud JOIN themes th ON ud.theme = th.id WHERE ud.id = ?';
+    $result = ejecutarQuery($sql, [$userId]);
+    return $result[0]["name"] ?? null;
+}
+/**
+ * Esta función devuelve el language-name a partir del id de usuario
+ */
+function getLanguageNameByUserId ($userId) {
+    $sql = 'SELECT l.name FROM users_data ud JOIN languages l ON ud.theme = l.id WHERE ud.id = ?';
+    $result = ejecutarQuery($sql, [$userId]);
+    return $result[0]["name"] ?? null;
+}
+/**
+ * Esta función devuelve toda la información de la tabla themes
+ */
+function getAllThemes () {
+    $sql = 'SELECT * FROM themes';
+    $result = ejecutarQuery($sql, []);
+    return $result;
+} 
+/**
+ * Esta función devuelve toda la información de la tabla languages
+ */
+function getAllLanguages () {
+    $sql = 'SELECT * FROM themes';
+    $result = ejecutarQuery($sql, []);
+    return $result;
+}  
 
 /**
  * Esta función sirve para comprobar si un role de usuario corresponde a un administrador
