709b48d38f2e9d488492da4fbf069b565ddd47b5
KurixDeCuatrocuernos <168125007+KurixDeCuatrocuernos@users.noreply.github.com>
2026-04-24 21:06:03 +0200

    Creación de Dashboard.jsx
    Se creó la página principal del frontend

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+const Dashboard = () => {
+
+    return (<div className="bg-light">
+        <h1 className='text-3xl font-bold text-center text-primary'>Hola, este es el inicio</h1>
+    </div>);
+}
+
+export default Dashboard;
