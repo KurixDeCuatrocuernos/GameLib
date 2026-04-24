bc6e2242cbaa95ac9ee15ca65c0e74a9d09b8767
KurixDeCuatrocuernos <168125007+KurixDeCuatrocuernos@users.noreply.github.com>
2026-04-24 21:01:19 +0200

    Creación del Routing
    Se creó el enrutado del frontend en main.jsx y App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
-import './App.css'
+// Importamos funciones
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+// Importamos nuestras páginas
+import Navbar from './components/Navbar'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold text-center'>Hola, este es el inicio</h1>
+    <Navbar />
+    {/* Rutas de nuestro Frontend */}
+      <Routes>
+        {/* Inicio */}
+        <Route path="/" element={<Dashboard />} /> 
+        {/* Login */}
+        <Route path='/login' element={<Login />} />
+        {/* Registro */}
+        <Route path='/signup' element={<Signup />} />
+        
+      </Routes>
     </>
   )
 }
